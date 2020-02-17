d35fe44d14d74fdccaf7f26174393590c843be32
M Edoka (21750647) <21750647@dut4life.ac.za>
2020-02-17 15:37:56 +0200

    Add project files.

---
+++ Dealership/Model/Milleage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dealership.Model
+{
+    public class Milleage
+    {
+        public int Upper { get; set; }
+        public int Lower { get; set; }
+        public decimal PercentageCharged { get; set; }
+
+        public Milleage() { }
+
+        public Milleage(int upper, int lower,decimal percentagecharge)
+        {
+            Upper = upper;
+            Lower = lower;
+            PercentageCharged = percentagecharge;
+        }
+    }
+}
---
+++ Dealership/Model/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dealership.Model
+{
+    public enum ServiceHistory 
+    {
+    Full,
+    Medium,
+    None
+    }
+
+    public enum Spec 
+    {
+    
+    
+    }
+    public class Vehicle
+    {
+        public double Millege { get; set; }
+        public List<string> Specs { get; set; }
+        public List<string> ServiceHistory { get; set; }
+        public double BookValue { get; set; }
+        public int Year { get; set; }
+        public List<string> Color { get; set; }
+
+  
+        public Vehicle(double millege,List<string>spec, List<string> servicehistory, double bookvalue,int year,List<string> color)
+        {
+            Millege = millege;
+            Specs = spec;
+            ServiceHistory = servicehistory;
+            BookValue = bookvalue;
+            Year = year;
+            Color = color;
+        }
+    }
+}
